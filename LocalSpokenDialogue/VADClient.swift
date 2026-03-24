a38875446e6d058774ab77ed014af34ef366a0a9
morik <kosuke.mori.5120@gmail.com>
2026-03-24 22:21:55 +0900

    add vad client

--- LocalSpokenDialogue/VADClient.swift
+++ LocalSpokenDialogue/VADClient.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import OnnxRuntimeBindings
+
+final class VADClient {
+    private var session: ORTSession?
+    private var state: [Float] = Array(repeating: 0, count: 2 * 1 * 128)
+
+    func load() throws {
+        let modelURL = Bundle.main.url(
+            forResource: "silero_vad_op18_ifless",
+            withExtension: "onnx"
+        )!
+        
+        let env = try ORTEnv(loggingLevel: .warning)
+        let options = try ORTSessionOptions()
+        try options.setIntraOpNumThreads(1)
+        session = try ORTSession(
+            env: env,
+            modelPath: modelURL.path,
+            sessionOptions: options
+        )
+    }
+
+    func reset() {
+        state = Array(repeating: 0, count: 2 * 1 * 128)
+    }
+}
